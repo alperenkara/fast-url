649b3dca2a6faa0aa3530db3a21d79a21f5d62b8
Alperen Kara <alprnkr@gmail.com>
2022-06-13 21:41:43 +0000

    readme, pylint

--- app/config.py
+++ app/config.py
@@ -1,10 +1,13 @@
 # app/config.py
 
 
-from pydantic import BaseSettings
+from pydantic import BaseSettings # pylint: disable=import-error
 
-
-class Settings(BaseSettings):
+"""
+a subclass of BaseSettings. The BaseSettings class comes in handy to define environment variables in your application. You only have to define the variables that you want to use, and pydantic takes care of the rest. 
+In other words, pydantic will automatically assume those default values if it doesn’t find the corresponding environment variables.
+"""
+class Settings(BaseSettings): 
 
     env_name: str = "Local"
 
@@ -13,7 +16,7 @@ class Settings(BaseSettings):
     db_url: str = "sqlite:///./shortener.db"
 
 
-def get_settings() -> Settings:
+def get_settings() -> Settings: 
 
     settings = Settings()
 
